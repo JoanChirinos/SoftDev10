a748ac164c0e3b1974acbba3d7d47fd532283a9a
JoanChirinos <jchirinos@stuy.edu>
2019-02-06 14:09:48 -0500

    actually done according to mr brown's standards

--- 04_canvas-dvd/animate.js
+++ 04_canvas-dvd/animate.js
@@ -1,5 +1,3 @@
-
-
 //model for HTML5 canvas-based animation
 
 //access canvas and buttons via DOM
@@ -8,7 +6,7 @@ var dotButton = document.getElementById("circle");
 
 var dvdButton = document.getElementById('dvd');
 
-var stopButton = document.getElementById( "stop" );
+var stopButton = document.getElementById("stop");
 
 //prepare to interact with canvas in 2D
 var ctx = c.getContext("2d");
@@ -23,97 +21,86 @@ var requestID;
 var radius = 0;
 var growing = true;
 
-var logo = new Image();
-logo.src = "logo_dvd.jpg";
+var drawDot = function () {
+  window.cancelAnimationFrame(requestID);
 
-var rectWidth = 100;
-var rectHeight = 50;
+  ctx.clearRect(0, 0, c.width, c.height);
 
-var xVel, yVel, rectX, rectY;
-
-var drawDot = function() {
-  window.cancelAnimationFrame( requestID );
-
-  ctx.clearRect( 0, 0, c.width, c.height );
-
-  if ( growing ) {
+  if (growing) {
     radius += 1;
-  }
-  else {
+  } else {
     radius -= 1;
   }
 
-  if ( radius == (c.width / 2) )
-  growing = false;
-  else if ( radius == 0 ) {
+  if (radius == (c.width / 2))
+    growing = false;
+  else if (radius == 0) {
     growing = true;
   }
 
   //draw the dot
   ctx.beginPath();
-  ctx.arc( c.width / 2, c.height / 2, radius, 0, 2 * Math.PI );
+  ctx.arc(c.width / 2, c.height / 2, radius, 0, 2 * Math.PI);
   ctx.stroke();
   ctx.fill();
 
-  requestID = window.requestAnimationFrame( drawDot );
+  requestID = window.requestAnimationFrame(drawDot);
 };
 
 
-var stopIt = function() {
-  console.log( requestID );
-  window.cancelAnimationFrame( requestID );
+var stopIt = function () {
+  console.log(requestID);
+  window.cancelAnimationFrame(requestID);
 };
 
-var drawDvd = function() {
-  window.cancelAnimationFrame( requestID );
 
-  ctx.clearRect( 0, 0, c.width, c.height );
-  
-  if (rectX == 0 || rectX == c.width - rectWidth) {
-    xVel *= -1;
-  }
-  if (rectY == 0 || rectY == c.height - rectHeight) {
-    yVel *= -1;
-  }
-  
-  rectX += xVel;
-  rectY += yVel;
-
-  //draw the dvd
-  ctx.drawImage(logo, rectX, rectY, rectWidth, rectHeight);
-
-  // animateboi
-  requestID = window.requestAnimationFrame( drawDvd );
-};
 
 var dvdLogoSetup = function () {
   window.cancelAnimationFrame(requestID);
-  
-  rectX = Math.floor(Math.random() * (c.width - rectWidth));
-  rectY = Math.floor(Math.random() * (c.height - rectHeight));
-  
-  xVel = 1;
-  yVel = 1;
-  
-  requestID = window.requestAnimationFrame( drawDvd );
+
+  var rectWidth = 100;
+  var rectHeight = 50;
+
+  var rectX = Math.floor(Math.random() * (c.width - rectWidth));
+  var rectY = Math.floor(Math.random() * (c.height - rectHeight));
+
+  var xVel = 1;
+  var yVel = 1;
+
+  var logo = new Image();
+  logo.src = "logo_dvd.jpg";
+
+  var dvdLogo = function () {
+    window.cancelAnimationFrame(requestID);
+
+    ctx.clearRect(0, 0, c.width, c.height);
+
+    if (rectX <= 0 || rectX >= c.width - rectWidth) {
+      xVel *= -1;
+    }
+    if (rectY <= 0 || rectY >= c.height - rectHeight) {
+      yVel *= -1;
+    }
+
+    rectX += xVel;
+    rectY += yVel;
+
+    //draw the dvd
+    ctx.drawImage(logo, rectX, rectY, rectWidth, rectHeight);
+
+    // animateboi
+    requestID = window.requestAnimationFrame(dvdLogo);
+  };
+
+  dvdLogo();
 };
 
-dotButton.addEventListener( "click", drawDot );
-stopButton.addEventListener( "click",  stopIt );
+dotButton.addEventListener("click", drawDot);
+stopButton.addEventListener("click", stopIt);
 dvdButton.addEventListener('click', dvdLogoSetup)
 
 
 // HOMEWORK 4 STUFF
-
-
-
-
-
-
-
-
-
-
 
 
 
