524a48839b7686354799dbeb5299b3c4cbbba19f
JoanChirinos <jchirinos3201@gmail.com>
2019-04-28 10:39:51 -0400

    beanbois

--- 20/work.py
+++ 20/work.py
@@ -6,25 +6,29 @@ def freq(word):
     raw = ''
     with open(INPUT_FILE) as f:
         raw = f.read().lower()
-    return len([x for x in raw.split() if x == word.lower()])
+    return reduce(lambda x, y: x + 1 if y == word else x,\
+                  raw.split(),\
+                  0)
+    
 
-def group_freq(words):
+def group_freq(phrase):
     raw = ''
     with open(INPUT_FILE) as f:
         raw = f.read().lower()
-    return len([x for x in raw.split() if x in map(lambda x: x.lower(), words)])
+    return reduce(lambda x, y: x + 1 if raw[y:y+len(phrase)] == phrase else x,\
+                  range(len(raw) - len(phrase)),\
+                  0)
+
+def add_one(d, word):
+    d[word] += 1
+    return d
 
 def most_freq():
     raw = ''
     with open(INPUT_FILE) as f:
         raw = f.read().lower()
-    d = {}
-    for word in raw.split():
-        if word in d:
-            d[word] += 1
-        else:
-            d[word] = 1
-    l = []
-    for word in d:
-        l.append((word, d[word]))
-    return sorted(l, key=lambda x: x[1], reverse=True)[:20]
+    d =  reduce(lambda x, y: add_one(x, y), raw.split(), dict.fromkeys(set(raw.split()), 0))
+    return sorted([(x, d[x]) for x in d], key=lambda x: x[1], reverse=True)[0]
+    #return reduce(lambda x, y: (y, d[y]) if d[y] > x[1] else x,\
+    #              d,\
+    #              ('', -1))[0]
